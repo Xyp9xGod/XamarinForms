99e8274237751c426a8ac49802479bdc1e821b4b
Arilson Silva <arilson.silva@univille.br>
2018-12-23 18:29:14 -0200

    App para Busca de CEP nos correios.

---
+++ Seção 05/App01_ConsultaCEP/App01_ConsultaCEP/App01_ConsultaCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_ConsultaCEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace App01_ConsultaCEP.Servico
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);//troca o cep informado no param{0} da url
+
+            WebClient wc = new WebClient();//instancia client para fazer download do conteudo da pagina
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);//Desserializa o obj para json.
+
+            if (end == null) return null;
+
+            return end;
+        }
+    }
+}
